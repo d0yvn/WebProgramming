41e49485a6d5244bae828ba4e70dcbb75c92306b
JaeJun <jejun97@gmail.com>
2021-05-24 18:50:14 +0900

    submit관련 플레그 변수 수정

--- js/recommend.js
+++ js/recommend.js
@@ -5,20 +5,22 @@ const step3 = document.querySelector('.step-3');
 let step1_cnt = 0;
 let step2_cnt = 0;
 let step3_cnt = 0;
-
-
-const submit = document.querySelector('input[type="submit"]');
 // submit 버튼 비활성화
+const form = document.querySelector('form');
+
 $("input[type='submit']").click(function(event) {
     event.preventDefault();
 })
 
 window.onclick = function(event) {
+    let success = false;
     console.log(event.target);
+
     // checked btn -> 'selected' class toggle시켜주기
     if (event.target.matches('.dropdown-item')) {
-        // event.target.parentElement.parentElement.previousElementSibling.classList.toggle('selected');
+        // do noting.
     } else if (event.target.matches('.rcp-thm')) {
+        console.log(step2.getElementsByClassName('selected').length);
         event.target.classList.toggle('selected');
     } else if (event.target.matches('.rcp-time')) {
         event.target.classList.toggle('selected');
@@ -26,7 +28,7 @@ window.onclick = function(event) {
 
     /*  */
     if (event.target.matches('.submit')) {
-        step1_cnt = step1.getElementsByClassName('selected').length / 2;
+        step1_cnt = step1.getElementsByClassName('selected').length;
         step2_cnt = step2.getElementsByClassName('selected').length;
         step3_cnt = step3.getElementsByClassName('selected').length;
         if (step1_cnt == 0) {
@@ -35,13 +37,16 @@ window.onclick = function(event) {
             window.alert('Please check your recipe theme')
         } else if (step3_cnt == 0) {
             window.alert('Please check recipe time you want')
+        } else if (step3_cnt > 1) {
+            window.alert('Please choose just one at step3')
+        } else {
+            success = true;
         }
 
-
-        /* 위에 체크 잘 되어있으면 submit 버튼 활성화 */
-        $("input[type='submit']").click(function(event) {
-            return true; /* revert submit button to original situation */
-        })
+        if (success) {
+            success = false;
+            form.submit();
+        }
 
     }
 
