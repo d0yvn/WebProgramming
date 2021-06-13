bb965e8d4005dc93dc79b4888d2e44ae599b0917
yellowtoast <kjh9519@naver.com>
2021-06-13 10:28:34 +0900

    editted

--- index.js
+++ index.js
@@ -50,7 +50,6 @@ app.use('/recommend', require('./routes/recommend'));
 app.use('/posts', util.getPostQueryString, require('./routes/posts'));
 app.use('/users', require('./routes/users'));
 app.use('/comments', util.getPostQueryString, require('./routes/comments'));
-app.use('/mypage',require('./routes/mypage'));
 app.use('/files', require('./routes/files')); // 1
 
 
--- routes/MyPage.js
+++ routes/MyPage.js
@@ -1,28 +1,28 @@
-const mypage = require('../models/mypage.js')
+//const mypage = require('../models/mypage.js')
 const foods = require('../models/foods.js')
 
 var express = require('express');
 var router = express.Router();
 
-router.use('/saveRecipe',function(req,res){ //마이페이지에 레시피 저장.
-  mypage.findOneAndUpdate(
-      {userid:req.session.userId},
-      {$push : {foods:req.body.recipeInfo}},
-      {upsert: true},(err,result)=>{
-      console.log(result);
-  });
-  next();
-});
+// router.use('/saveRecipe',function(req,res){ //마이페이지에 레시피 저장.
+//   mypage.findOneAndUpdate(
+//       {userid:req.session.userId},
+//       {$push : {foods:req.body.recipeInfo}},
+//       {upsert: true},(err,result)=>{
+//       console.log(result);
+//   });
+//   next();
+// });
 
-router.get('/show',function(req,res){ //마이페이지에 저장된 나의 레시피 확인.
-  mypage.findOne({"userid" : req.session.userId}).populate('foods').exec(function(err,data){
-      var food = new Array;
-      for(var i = 0;i<data.foods.length;i++){
-          food.push(data.foods[i])
-      }
-      console.log(food);
-      res.render('/mypage/mypage',{food});
-  })
-})
+// router.get('/show',function(req,res){ //마이페이지에 저장된 나의 레시피 확인.
+//   mypage.findOne({"userid" : req.session.userId}).populate('foods').exec(function(err,data){
+//       var food = new Array;
+//       for(var i = 0;i<data.foods.length;i++){
+//           food.push(data.foods[i])
+//       }
+//       console.log(food);
+//       res.render('/mypage/mypage',{food});
+//   })
+// })
 
-module.exports = router;
+// module.exports = router;
